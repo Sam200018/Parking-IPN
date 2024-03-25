229f3c45db43c2de0df0872a2eb0ecf1e3fc4fc4
Samuel Bautista <samuelbaubas@gmail.com>
2024-03-25 15:52:01 -0600

    Add LoginScreen ui layer

---
+++ Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/ui/main/views/LoginScreen.kt
@@ -0,0 +1,160 @@
+package mx.ipn.escom.bautistas.parking.ui.main.views
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import mx.ipn.escom.bautistas.parking.R
+import mx.ipn.escom.bautistas.parking.ui.components.ButtonComponent
+import mx.ipn.escom.bautistas.parking.ui.components.TextFieldComponent
+import mx.ipn.escom.bautistas.parking.ui.main.viewmodels.AuthViewModel
+import mx.ipn.escom.bautistas.parking.ui.main.viewmodels.LoginViewModel
+
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier,
+    windowSizeClass: WindowSizeClass,
+    authViewModel: AuthViewModel,
+) {
+    val loginViewModel: LoginViewModel = viewModel()
+
+
+    Scaffold(
+        modifier.fillMaxSize()
+    ) {
+        Box(modifier.padding(it)) {
+            when {
+                windowSizeClass.widthSizeClass == WindowWidthSizeClass.Medium -> CompactLogin(
+                    loginViewModel = loginViewModel,
+                    authViewModel = authViewModel
+                )
+
+                windowSizeClass.widthSizeClass >= WindowWidthSizeClass.Expanded -> ExpandedLogin(
+                    loginViewModel = loginViewModel,
+                    authViewModel = authViewModel
+                )
+
+                else -> ExpandedLogin(
+                    loginViewModel = loginViewModel,
+                    authViewModel = authViewModel
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CompactLogin(
+    modifier: Modifier = Modifier,
+    padding: Dp = 25.dp,
+    loginViewModel: LoginViewModel,
+    authViewModel: AuthViewModel,
+) {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = padding)
+    ) {
+
+        Text(
+            text = stringResource(id = R.string.labelLogin),
+            fontSize = 32.sp,
+            color = colorResource(
+                id = R.color.guinda
+            )
+        )
+        Spacer(modifier = modifier.height(95.dp))
+        TextFieldComponent(
+            modifier.fillMaxWidth(),
+            value = loginViewModel.emailInput,
+            label = stringResource(id = R.string.email_label)
+        ) {
+            loginViewModel.onEmailChange(it)
+        }
+        TextFieldComponent(
+            modifier.fillMaxWidth(),
+            value = loginViewModel.passwordInput,
+            label = stringResource(id = R.string.password_label)
+        ) {
+            loginViewModel.onPasswordChange(it)
+        }
+        Spacer(modifier = modifier.height(90.dp))
+        ButtonComponent(
+            modifier = modifier.width(250.dp),
+            label = stringResource(id = R.string.login_button_label)
+        ) {
+            Log.i("login S","hola")
+            authViewModel.doLogin(loginViewModel.emailInput, loginViewModel.passwordInput)
+        }
+    }
+}
+
+@Composable
+fun ExpandedLogin(
+    modifier: Modifier = Modifier,
+    loginViewModel: LoginViewModel,
+    authViewModel: AuthViewModel
+) {
+    Row(modifier = modifier.fillMaxSize()) {
+        Box(
+            modifier
+                .background(color = colorResource(id = R.color.grey_figma))
+                .fillMaxHeight()
+                .weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo_parking),
+                    contentDescription = ""
+                )
+                Text(
+                    text = stringResource(id = R.string.system_label),
+                    fontSize = 24.sp,
+                    color = colorResource(
+                        id = R.color.guinda
+                    )
+                )
+            }
+        }
+        Box(
+            modifier
+                .fillMaxHeight()
+                .weight(1f),
+        ) {
+            CompactLogin(modifier, padding = 90.dp, loginViewModel = loginViewModel, authViewModel)
+        }
+    }
+}
+
+
