a635b52b80b5f5abe56c0a82c203f5405d476d2a
Samuel Bautista <samuelbaubas@gmail.com>
2024-04-05 13:12:43 -0600

    Change Auth VM to get login response and show the result on UI

--- Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/ui/main/viewmodels/AuthViewModel.kt
+++ Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/ui/main/viewmodels/AuthViewModel.kt
@@ -4,10 +4,14 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import mx.ipn.escom.bautistas.parking.data.auth.AuthRepository
 import mx.ipn.escom.bautistas.parking.model.AuthResponse
 import mx.ipn.escom.bautistas.parking.model.LoginRequest
+import mx.ipn.escom.bautistas.parking.ui.main.interactions.AuthState
+import mx.ipn.escom.bautistas.parking.ui.main.interactions.AuthStatus
 import retrofit2.HttpException
 import java.io.IOException
 import java.lang.RuntimeException
@@ -17,7 +21,8 @@ import javax.inject.Inject
 class AuthViewModel @Inject constructor(
     private val authRepository: AuthRepository
 ) : ViewModel() {
-
+    private val _authState = MutableStateFlow(AuthState())
+    val authState = _authState.asStateFlow()
 
     suspend fun login(email: String, password: String): Result<AuthResponse> {
         return try {
@@ -34,6 +39,32 @@ class AuthViewModel @Inject constructor(
     }
 
     fun doLogin(email: String, password: String) {
-        viewModelScope.launch { login(email, password) }
+        viewModelScope.launch {
+            val result = login(email, password)
+            if (result.isSuccess) {
+                setLoggedUser(result.getOrDefault(null))
+            } else {
+                val errorMessage = result.exceptionOrNull()?.message ?: "Error desconocido"
+                logout(message = errorMessage)
+            }
+        }
+    }
+
+    fun logout(message: String? = null) {
+        _authState.value = AuthState(
+            AuthStatus.Unauthenticated,
+            message = message
+        )
+    }
+
+
+    private fun setLoggedUser(response: AuthResponse?) {
+//        TODO: Save token to check status
+
+        _authState.value = AuthState(
+            message = "Inicio exitoso",
+            authStatus = AuthStatus.Authenticated,
+            account = response?.account
+        )
     }
 }
