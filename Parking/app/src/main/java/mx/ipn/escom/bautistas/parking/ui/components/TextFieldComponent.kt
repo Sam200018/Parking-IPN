0e99be27b41e90265e9b9499dcb334c686f51645
Samuel Bautista <samuelbaubas@gmail.com>
2024-04-09 11:33:08 -0600

    Update Text Field component

--- Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/ui/components/TextFieldComponent.kt
+++ Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/ui/components/TextFieldComponent.kt
@@ -4,16 +4,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import mx.ipn.escom.bautistas.parking.R
 
 @Composable
 fun TextFieldComponent(
     modifier: Modifier = Modifier,
+    icon: ImageVector? = null,
     value: String = "",
     label: String = "",
     keyboardOptions: KeyboardOptions = KeyboardOptions(),
@@ -24,18 +30,26 @@ fun TextFieldComponent(
 ) {
 
     Column {
-        TextField(
+        OutlinedTextField(leadingIcon = {
+            if (icon != null) {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = label,
+                    tint = colorResource(id = R.color.guinda)
+                )
+            }
+        },
             value = value,
             onValueChange = onChanged,
             label = {
-                Text(text = label)
+                Text(text = label, color = colorResource(id = R.color.guinda))
             },
             readOnly = readOnly,
             modifier = modifier,
             keyboardOptions = keyboardOptions,
             isError = isError,
             supportingText = {
-                if(isError){
+                if (isError) {
                     Text(text = errorMessage)
                 }
             }
@@ -44,8 +58,8 @@ fun TextFieldComponent(
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 private fun TextFieldPrev() {
-    TextFieldComponent(){}
+    TextFieldComponent() {}
 }
