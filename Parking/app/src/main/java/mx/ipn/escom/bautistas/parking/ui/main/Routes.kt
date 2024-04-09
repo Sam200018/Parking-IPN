8fbc3fe724b92fbc5331857a193a3bd6500ad6c3
Samuel Bautista <samuelbaubas@gmail.com>
2024-04-09 11:25:36 -0600

    Add new routes in Routes.kt

--- Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/ui/main/Routes.kt
+++ Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/ui/main/Routes.kt
@@ -6,4 +6,7 @@ sealed class Routes(
     data object SplashScreen : Routes(route = "/")
     data object LoginScreen : Routes(route = "/login")
     data object HomeScreen : Routes(route = "/home")
+    data object UserSelect : Routes(route = "/user_select")
+    data object NewUser : Routes(route = "/new_user")
+    data object UserAvailable : Routes(route = "/user_available")
 }
