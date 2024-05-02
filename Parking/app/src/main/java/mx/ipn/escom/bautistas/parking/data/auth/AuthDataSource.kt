7c8c3d4e8760d4588d124fedfb0ac3b2fbf71cac
Samuel Bautista <samuelbaubas@gmail.com>
2024-05-01 21:46:27 -0600

    Add check status method to AuthDataSource

--- Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/data/auth/AuthDataSource.kt
+++ Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/data/auth/AuthDataSource.kt
@@ -3,9 +3,13 @@ package mx.ipn.escom.bautistas.parking.data.auth
 import mx.ipn.escom.bautistas.parking.model.AuthResponse
 import mx.ipn.escom.bautistas.parking.model.LoginRequest
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface AuthDataSource {
     @POST("login")
     suspend fun login(@Body loginRequest: LoginRequest ): AuthResponse
+
+    @POST("check-status")
+    suspend fun checkStatus(@Header("Authorization")token:String): AuthResponse
 }
