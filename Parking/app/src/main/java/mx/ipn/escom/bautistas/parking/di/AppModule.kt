17fbabdc6e1e17bedab3eb7fc34fbabe371cde6b
Samuel Bautista <samuelbaubas@gmail.com>
2024-05-01 21:48:51 -0600

    Add room config to appModule

--- Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/di/AppModule.kt
+++ Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/di/AppModule.kt
@@ -1,6 +1,7 @@
 package mx.ipn.escom.bautistas.parking.di
 
 import android.content.Context
+import androidx.room.Room
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
@@ -11,13 +12,19 @@ import mx.ipn.escom.bautistas.parking.config.PropertiesManager
 import mx.ipn.escom.bautistas.parking.data.auth.AuthDataSource
 import mx.ipn.escom.bautistas.parking.data.auth.AuthRepository
 import mx.ipn.escom.bautistas.parking.data.auth.AuthRepositoryImpl
+import mx.ipn.escom.bautistas.parking.data.token.UserDao
+import mx.ipn.escom.bautistas.parking.data.user.UserDataSource
+import mx.ipn.escom.bautistas.parking.data.user.UserRepository
+import mx.ipn.escom.bautistas.parking.data.user.UserRepositoryImpl
+import mx.ipn.escom.bautistas.parking.model.AppDatabase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 
-@InstallIn(SingletonComponent::class)
 @Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
     //    properties
@@ -48,6 +55,31 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun providesAuthRepository(authDataSource: AuthDataSource): AuthRepository =
-        AuthRepositoryImpl(authDataSource)
+    fun providesUserDataSource(retrofit: Retrofit): UserDataSource {
+        return retrofit.create(UserDataSource::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providesAuthRepository(authDataSource: AuthDataSource, userDao: UserDao): AuthRepository =
+        AuthRepositoryImpl(authDataSource,userDao)
+
+
+    @Provides
+    @Singleton
+    fun providesUserRepository(userDataSource: UserDataSource): UserRepository {
+        return UserRepositoryImpl(userDataSource)
+    }
+
+
+    @Provides
+    @Singleton
+    fun providesUserTokenRoomDatabase(@ApplicationContext app: Context): AppDatabase =
+        Room.databaseBuilder(
+            app, AppDatabase::class.java, "app_database"
+        ).fallbackToDestructiveMigration().build()
+
+    @Provides
+    @Singleton
+    fun providesNotesDao(appDatabase: AppDatabase): UserDao = appDatabase.userDao()
 }
