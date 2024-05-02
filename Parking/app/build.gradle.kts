6dfe0a359439efd85adc09d3cb67f5c243a15bfa
Samuel Bautista <samuelbaubas@gmail.com>
2024-05-01 21:44:20 -0600

    Add new dependencies to this project

--- Parking/app/build.gradle.kts
+++ Parking/app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.android.support:support-annotations:28.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -68,13 +69,13 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.12.0")
 //    Extended icons
     implementation("androidx.compose.material:material-icons-extended:1.6.5")
 //    lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 //    HCE
-    implementation ("com.google.android.gms:play-services-nearby:19.1.0")
+    implementation("com.google.android.gms:play-services-nearby:19.1.0")
 //    Window Size
     implementation(platform("androidx.compose:compose-bom:2024.03.00"))
     implementation("androidx.compose.material3:material3-window-size-class")
@@ -84,14 +85,28 @@ dependencies {
 //    Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
 //    Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.google.code.gson:gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.9.0")
 //    Retrofit with Scalar Converter
-    implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
 //    Serialization
-    implementation ("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+//    Camara X
+    implementation("androidx.camera:camera-camera2:1.3.2")
+    implementation("androidx.camera:camera-lifecycle:1.3.2")
+    implementation("androidx.camera:camera-view:1.3.2")
+    implementation("com.google.accompanist:accompanist-permissions:0.32.0")
+//    Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
+//      Room
+    val roomVersion = "2.6.0"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+    implementation ("androidx.room:room-ktx:$roomVersion")
+
+
 }
 
 kapt {
