612ae1d8f29b954fa01aac7205d40a9ee747464a
Samuel Bautista <samuelbaubas@gmail.com>
2024-05-01 21:48:55 -0600

    Add getToken, saveToken, checkStatus methods to AuthRepository

--- Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/data/auth/AuthRepository.kt
+++ Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/data/auth/AuthRepository.kt
@@ -1,17 +1,29 @@
 package mx.ipn.escom.bautistas.parking.data.auth
 
+import mx.ipn.escom.bautistas.parking.data.token.UserDao
 import mx.ipn.escom.bautistas.parking.model.AuthResponse
 import mx.ipn.escom.bautistas.parking.model.LoginRequest
+import mx.ipn.escom.bautistas.parking.model.UserToken
 import javax.inject.Inject
 
 interface AuthRepository {
     suspend fun login(loginRequest: LoginRequest): AuthResponse
+    suspend fun getToken(): String?
+    suspend fun saveToken(userToken: UserToken)
+    suspend fun checkStatus(token:String): AuthResponse
 }
 
 
 class AuthRepositoryImpl @Inject constructor(
-    private val authDataSource: AuthDataSource
+    private val authDataSource: AuthDataSource,
+    private val userDao: UserDao
 ) : AuthRepository {
     override suspend fun login(loginRequest: LoginRequest): AuthResponse =
         authDataSource.login(loginRequest)
+
+    override suspend fun getToken() = userDao.getToken()
+    override suspend fun saveToken(userToken: UserToken) = userDao.insertToken(userToken)
+    override suspend fun checkStatus(token: String) = authDataSource.checkStatus(token)
+
+
 }
