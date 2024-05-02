25377c6a69b5a73a848114af0ae5e30ad580152b
Samuel Bautista <samuelbaubas@gmail.com>
2024-05-01 21:52:02 -0600

    Change Cuenta Model to use Persona instead of Usuario

--- Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/model/Cuenta.kt
+++ Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/model/Cuenta.kt
@@ -7,18 +7,16 @@ import kotlinx.serialization.Serializable
 data class Cuenta (
     @SerializedName("id_cuenta")
     val idCuenta: Long,
-    @SerializedName("id_usuario")
-    val idUsuario: Long,
+    @SerializedName("id_persona")
+    val idPersona: Long,
     @SerializedName("id_rol")
     val idRol: Long,
     @SerializedName("id_prog_academico")
     val idProgAcademico: Long,
     val activo: Boolean,
     val correo: String,
-    @SerializedName("debe_cambiar_contrasena")
-    val debeCambiarContrasena: Boolean,
     val rol: Rol,
     @SerializedName("prog_academico")
     val progAcademico: ProgAcademico,
-    val usuario: Usuario
+    val persona: Persona
 )
