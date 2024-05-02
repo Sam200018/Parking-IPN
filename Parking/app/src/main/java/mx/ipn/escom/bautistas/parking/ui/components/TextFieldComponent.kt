dbbc725a3947c96e67b5112a0a1c4daf1b554c34
Samuel Bautista <samuelbaubas@gmail.com>
2024-05-01 22:01:27 -0600

    Add icon to TextFieldComponent

--- Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/ui/components/TextFieldComponent.kt
+++ Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/ui/components/TextFieldComponent.kt
@@ -4,13 +4,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,7 +16,7 @@ import mx.ipn.escom.bautistas.parking.R
 @Composable
 fun TextFieldComponent(
     modifier: Modifier = Modifier,
-    icon: ImageVector? = null,
+    icon: @Composable (()->Unit)? = null,
     value: String = "",
     label: String = "",
     keyboardOptions: KeyboardOptions = KeyboardOptions(),
@@ -30,15 +27,8 @@ fun TextFieldComponent(
 ) {
 
     Column {
-        OutlinedTextField(leadingIcon = {
-            if (icon != null) {
-                Icon(
-                    imageVector = icon,
-                    contentDescription = label,
-                    tint = colorResource(id = R.color.guinda)
-                )
-            }
-        },
+        OutlinedTextField(
+            leadingIcon = icon,
             value = value,
             onValueChange = onChanged,
             label = {
