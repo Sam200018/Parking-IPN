f3b3d518d47348e5fd6aaace95908b3737cb8fdd
Samuel Bautista <samuelbaubas@gmail.com>
2024-09-13 15:52:43 -0600

    New routes added

--- Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/ui/main/Routes.kt
+++ Parking/app/src/main/java/mx/ipn/escom/bautistas/parking/ui/main/Routes.kt
@@ -7,10 +7,12 @@ sealed class Routes(
     data object LoginScreen : Routes(route = "/login")
     data object HomeScreen : Routes(route = "/home")
     data object GenerateAccessCard : Routes(route = "/generate_access_card")
-    data object NewUser : Routes(route = "/new_user")
+    data object NewAccountUser : Routes(route = "/new_account_user")
     data object NewVehicle : Routes(route = "/new_vehicle")
+    data object NewUser : Routes(route = "/new_user")
 
     //  newUserScreen
+    data object NewAccountUserMainContent : Routes(route = "/new_account_user_main")
     data object NewUserMainContent : Routes(route = "/new_user_main")
     data object NewUserCamaraP : Routes(route = "/camara_person")
     data object NewUserCamaraI : Routes(route = "/camara_ID")
@@ -22,4 +24,9 @@ sealed class Routes(
     data object GenAccessCardMain : Routes(route = "/generate_access_card_main")
     data object CompleteUserInfo : Routes(route = "/completeUser")
 
+    //    Manual registration
+    data object ManualRegistration : Routes(route = "/manual_registration_main")
+    data object ManualRegistrationSelect : Routes(route = "/manual_registration_select")
+    data object ManualRegistrationVehicle : Routes(route = "/manual_registration_vehicle")
+    data object ManualRegistrationResult : Routes(route = "/manual_registration_result")
 }
