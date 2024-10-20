23c435d1d6acee948f0f6bd74af73e3b0d1fb0a5
Samuel Bautista <samuelbaubas@gmail.com>
2024-10-19 19:43:56 -0600

    add ids to migrations

--- Parking_backend/database/migrations/2024_10_20_002855_create_tarjetas_acceso_table.php
+++ Parking_backend/database/migrations/2024_10_20_002855_create_tarjetas_acceso_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('Tarjetas_Acceso', function (Blueprint $table) {
             $table->id('id_tarjeta_acceso');
-            $table->foreignId('id_cuenta')->nullable()->constrained('cuentas')->onDelete('cascade');
-            $table->foreignId('id_vehiculo')->constrained('vehiculos')->onDelete('cascade');
+            $table->foreignId('id_cuenta')->nullable()->constrained('Cuentas', 'id_cuenta')->onDelete('cascade');
+            $table->foreignId('id_vehiculo')->constrained('Vehiculos', 'id_vehiculo')->onDelete('cascade');
             $table->string('token', 255)->nullable();
         });
     }
